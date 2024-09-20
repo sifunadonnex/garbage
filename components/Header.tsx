6b3c3ab3cc5a3703b333c931ffbeee640d1a9b53
sifuna2002 <sifunado4704@gmail.com>
2024-09-20 10:25:25 +0300

    create user

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,7 @@ import { Badge } from "@/components/ui/badge";
 import { Web3Auth } from "@web3auth/modal"
 import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { createUser } from "@/utils/db/actions";
 
 const clientId = process.env.WEB3_AUTH_CLIENT_ID;
 
@@ -18,6 +19,66 @@ const chainConfig = {
     chainId: "0xaa36a7",
     rpcTarget: "https://rpc.ankr.com/eth_sepolia",
     displayName: "Sepolia",
-    tickerName: "SEPOLIA",
+    tickerName: "Ethereum",
     blockExplorer: "https://sepolia.etherscan.io",
+    ticker: "ETH",
+    logo: "https://assets.web3auth.io/evm/evm-logo.png",
 }
+
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+    config: chainConfig
+});
+
+const web3auth = new Web3Auth({
+    clientId,
+    web3AuthNetwork: WEB3AUTH_NETWORK.TESTNET,
+    privateKeyProvider,
+});
+
+interface HeaderProps {
+    onMenuClick: () => void;
+    totalEarnings: number;
+}
+
+export default function Header({ onMenuClick, totalEarnings }: HeaderProps) {
+    const [provider, setProvider] = useState<IProvider | null>(null);
+    const pathname = usePathname();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
+    const [userInfo, setUserInfo] = useState<any>(null);
+    const [balance, setBalance] = useState<number | null>(null);
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                await web3auth.initModal();
+                setProvider(web3auth.provider);
+
+                if(web3auth.connected) {
+                    setIsLoggedIn(true);
+                    const userInfo = await web3auth.getUserInfo();
+                    setUserInfo(userInfo);
+                    
+                    if(userInfo.email) {
+                        localStorage.setItem('userEmail', userInfo.email);
+                        try {
+                            await createUser(userInfo.email, userInfo?.name || 'Anonymous User');
+                        } catch (error) {
+                            console.error('Error creating user', error);
+                        }
+                    }
+                }
+            } catch (error) {
+                console.error("Error initializing Web3Auth:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        init();
+    }, []);
+
+
+    
+
